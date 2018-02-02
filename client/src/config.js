591c362cc8f8d50309583ad0f4947bd63ddefc3a
Nicci Williams <nicci@nicciwill.com>
2018-02-02 18:07:06 -0500

    ready to deploy

--- client/src/config.js
+++ client/src/config.js
@@ -3,3 +3,6 @@ export const API_BASE_URL = process.env.NODE_ENV === "production" ? process.env.
 //create-react-app does the following:
 //npm start - default process.env.NODE_ENV is development
 //npm build - default process.env.NODE_ENV is production
+
+export const CLIENT_ORIGIN =
+process.env.CLIENT_ORIGIN || 'http://localhost:3000'
