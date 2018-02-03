95f74f86f59d7c00db9e126a6ffb5513651622dd
Nicci Williams <nicci@nicciwill.com>
2018-02-03 01:43:53 -0500

    Queues functioning properly

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,5 @@
-export const API_BASE_URL = process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_BASED_URL : 'http://localhost:8080/api';
+export const API_BASE_URL = 
+process.env.NODE_ENV === "production" ? process.env.REACT_APP_API_BASE_URL : 'http://localhost:8080/api';
 
 //create-react-app does the following:
 //npm start - default process.env.NODE_ENV is development
